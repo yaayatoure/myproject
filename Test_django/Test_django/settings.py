8e22eedfdc44b6e03d572b3829cf42cfd08bd106
YAXYE CABDISALAN MOH <yaxyecabdisalanmohamed1234@gmail.com>
2025-07-07 10:53:01 +0300

    hello

--- Test_django/Test_django/settings.py
+++ Test_django/Test_django/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #session settings
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
